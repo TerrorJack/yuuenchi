0ef4806530564f21a5e305c8493bc99ba735959a
Shao Cheng <cheng.shao@tweag.io>
2018-10-29 06:45:45 +0800

    Simpler implementation of oneshot

--- src/OneShotIO.hs
+++ src/OneShotIO.hs
@@ -2,19 +2,10 @@ module OneShotIO
   ( oneshot
   ) where
 
-import Control.Concurrent
-import Control.Monad
-import Data.Function
+import System.IO.Unsafe
 
 {-# INLINE oneshot #-}
 oneshot :: IO a -> IO (IO a)
 oneshot m = do
-  ref <- newEmptyMVar
-  putMVar ref $ do
-    a <- m
-    putMVar ref $
-      fix $ \go -> do
-        putMVar ref go
-        pure a
-    pure a
-  pure $ join $ takeMVar ref
+  a <- unsafeInterleaveIO m
+  pure $ pure a
